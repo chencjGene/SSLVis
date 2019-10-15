19f96a365429d7b8ea19827de7ec1d18c3e91307
wangzw <wangzw834@163.com>
2019-10-15 10:42:12 +0800

    debug

--- application/data_transfer.py
+++ application/data_transfer.py
@@ -1,13 +1,17 @@
 import paramiko
 import shutil
 from scp import SCPClient
-
+# * 1. If you have no ssh private key, use "ssh-keygen -p -m PEM -f ~/.ssh/id_rsa" to generate.
+#       No passphrase for convenience!
+#       If you already have ssh private key, skip this step otherwise your old key will be replaced.
+# * 2. Use "ssh-copy-id yourusername@server" to send your public key to server.
+# * 3. config user, ip and remote_path in this file. Note: default local path is './data
 ssh = paramiko.SSHClient()
 user = 'zhaowei'
 ip = '166.111.80.25'
 remote_path = '/home/zhaowei/'
 ssh.load_system_host_keys()
-ssh.connect(hostname=ip, username='zhaowei', password='123456')
+ssh.connect(hostname=ip, username='zhaowei')
 def upload():
     with SCPClient(ssh.get_transport()) as scp:
         ssh.exec_command('rm -r '+remote_path+'data')
