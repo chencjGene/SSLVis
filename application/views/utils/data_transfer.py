b4b1ada47bb9f1bd5611dfd8f1dee66fb5aa96c0
wangzw <wangzw834@163.com>
2019-10-15 11:09:55 +0800

    change path

--- application/views/utils/data_transfer.py
+++ application/views/utils/data_transfer.py
@@ -5,22 +5,23 @@ from scp import SCPClient
 #       No passphrase for convenience!
 #       If you already have ssh private key, skip this step otherwise your old key will be replaced.
 # * 2. Use "ssh-copy-id yourusername@server" to send your public key to server.
-# * 3. config user, ip and remote_path in this file. Note: default local path is './data
+# * 3. config user, ip and remote_path in this file. Note: default local path is 'SSLVis/application/data'
 ssh = paramiko.SSHClient()
 user = 'zhaowei'
 ip = '166.111.80.25'
 remote_path = '/home/zhaowei/'
+local_path = '../../'
 ssh.load_system_host_keys()
 ssh.connect(hostname=ip, username='zhaowei')
 def upload():
     with SCPClient(ssh.get_transport()) as scp:
         ssh.exec_command('rm -r '+remote_path+'data')
-        scp.put('data', recursive=True, remote_path=remote_path)
+        scp.put(local_path+'data', recursive=True, remote_path=remote_path)
 
 def download():
     with SCPClient(ssh.get_transport()) as scp:
-        shutil.rmtree(path='./data')
-        scp.get(local_path='./', recursive=True, remote_path=remote_path+'data')
+        shutil.rmtree(path=local_path+'./data')
+        scp.get(local_path=local_path, recursive=True, remote_path=remote_path+'data')
 
 # upload or download
-upload()
+download()
