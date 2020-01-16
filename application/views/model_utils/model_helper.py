7a030a4a46c17526daa50903b0ef87b73cd287dd
chencjGene <chencjGene@gmail.com>
2020-01-16 19:42:04 +0800

    link hover function

--- application/views/model_utils/model_helper.py
+++ application/views/model_utils/model_helper.py
@@ -31,7 +31,7 @@ def build_laplacian_graph(affinity_matrix):
 
 
 def propagation(graph_matrix, affinity_matrix, train_y, alpha=0.2, max_iter=30,
-                tol=0.02, process_record=False, normalized=False):
+                tol=0.01, process_record=False, normalized=False):
     y = np.array(train_y)
     # label construction
     # construct a categorical distribution for classification only
