a608edc0c7d6dcbe40e1c876996c8269d6112dbe
zhaowei <wangzw834@163.com>
2020-02-11 15:52:45 +0800

    finish reconstruction

--- application/views/graph_utils/anchor_r.py
+++ application/views/graph_utils/anchor_r.py
@@ -34,6 +34,11 @@ class Anchors:
         self.tsne = None
         self.full_x = None
         self.entropy = None
+        self.old_nodes_id = []
+        self.old_nodes_tsne = None
+        self.hierarchy_info = None
+        self.remove_ids = []
+        self.data_degree = None
 
     # added by Changjian
     # link this class to SSLModel and Data
@@ -44,7 +49,12 @@ class Anchors:
         self.hierarchy_info_path = os.path.join(self.selected_dir, "hierarchy_info" + config.pkl_ext)
         self.tsne_path = os.path.join(self.selected_dir, "tsne.npy")
         self.full_x = self.data.get_full_train_X()
-        self.entropy = self.get_entropy(self.model.process_data)
+        self.old_nodes_id = []
+        self.old_nodes_tsne = None
+        self.entropy = None
+        self.tsne = None
+        self.hierarchy_info = None
+        self.data_degree = None
 
     def get_pred_labels(self):
         labels = self.model.get_pred_labels()
@@ -69,8 +79,7 @@ class Anchors:
         np.save(self.tsne_path, self.tsne)
 
     def tsne_evaluation(self, train_x_tsne):
-        while not self.model.simplification_end():
-            True
+        self.wait_for_simplify()
         node_num = train_x_tsne.shape[0]
         influence_matrix = self.model.simplified_affinity_matrix
         all_distance = 0
@@ -83,15 +92,22 @@ class Anchors:
                 all_distance += np.linalg.norm(train_x_tsne[i] - train_x_tsne[j], 2)
         print("Edge length sum:", all_distance)
 
+    def wait_for_simplify(self):
+        while not self.model.simplification_end():
+            True
+
     def get_hierarchical_sampling(self):
         if os.path.exists(self.hierarchy_info_path):
             with open(self.hierarchy_info_path, "rb") as f:
-                return pickle.load(f)
+                self.hierarchy_info = pickle.load(f)
         else:
+            if self.entropy == None:
+                self.entropy = self.get_entropy(self.model.process_data)
             hierarchical_info = self.construct_hierarchical_sampling(self.full_x, self.entropy, target_num=500)
             with open(self.hierarchy_info_path, "wb") as f:
                 pickle.dump(hierarchical_info, f)
-            return hierarchical_info
+            self.hierarchy_info = hierarchical_info
+        return self.hierarchy_info
 
     def construct_hierarchical_sampling(self, train_x: np.ndarray, entropy: np.ndarray, target_num: int):
         node_num = train_x.shape[0]
@@ -139,13 +155,161 @@ class Anchors:
         return entropy
 
     def get_data_area(self, ids = None, train_x_tsne = None):
-        pass
+        assert ids is not None or train_x_tsne is not None
+        if ids is not None:
+            data = self.get_train_x_tsne()[ids]
+        else:
+            data = train_x_tsne
+        min_x = float(np.min(data[:,0]))
+        min_y = float(np.min(data[:,1]))
+        max_x = float(np.max(data[:,0]))
+        max_y = float(np.max(data[:,1]))
+        area = {
+            "x": min_x,
+            "y": min_y,
+            "width": max_x-min_x,
+            "height": max_y-min_y
+        }
+        return area
 
-    def get_data_selection(self, area, level, old_nodes_ids, must_have_nodes):
-        pass
+    def get_data_selection(self, area, level, must_have_nodes):
+        # get level info
+        if self.hierarchy_info is None:
+            level_infos = self.get_hierarchical_sampling()
+        else:
+            level_infos = self.hierarchy_info
 
-    def re_tsne(self, selection, fixed_ids, fixed_tsne):
-        pass
+        # get data
+        train_x = self.full_x
+        train_x_tsne = self.tsne
 
-    def convert_to_dict(self):
-        pass
+        # get old data
+        old_nodes_ids = self.old_nodes_id
+        old_nodes_tsne = self.old_nodes_tsne
+
+        # get new graph
+        if level >= len(level_infos):
+            level = len(level_infos) - 1
+        if level == 0:
+            _selection = level_infos[level]['index']
+            _pos = train_x_tsne[_selection]
+        else:
+            _selection = []
+            last_level = level_infos[level-1]['index']
+            last_next = level_infos[level-1]['next']
+            last_pos = train_x_tsne[last_level]
+            tmp_cnt = 0
+            for i, ind in enumerate(last_level):
+                if int(ind) in old_nodes_ids:
+                    pos = old_nodes_tsne[old_nodes_ids.index(int(ind))]
+                else:
+                    pos = last_pos[i]
+                if area['x'] <= pos[0] <= area['x'] + area['width'] and area['y'] <= pos[1] <= area['y'] + area[
+                    'height']:
+                    tmp_cnt+=1
+                    _selection += last_next[i]
+            _pos = train_x_tsne[_selection]
+
+        selection = []
+        new_nodes = []
+        for i, ind in enumerate(_selection):
+                if int(ind) in old_nodes_ids:
+                    selection.append(int(ind))
+                else:
+                    new_nodes.append(int(ind))
+
+        for node_id in must_have_nodes:
+            if int(node_id) not in selection:
+                if int(node_id) in old_nodes_ids:
+                    selection.append(int(node_id))
+                else:
+                    new_nodes.append(int(node_id))
+
+        for id in self.remove_ids:
+            if id in selection:
+                selection.remove(id)
+            if id in new_nodes:
+                new_nodes.remove(id)
+        old_cnt = len(selection)
+        # add must_have_nodes
+        selection = list(dict.fromkeys(selection + new_nodes))
+        return selection, old_cnt
+
+    def re_tsne(self, selection, fixed_cnt = 0):
+        # get data
+        train_x = self.full_x
+        train_x_tsne = self.tsne
+
+        # get old data
+        old_nodes_ids = self.old_nodes_id
+        old_nodes_tsne = self.old_nodes_tsne
+
+        samples_x = train_x[selection]
+        init_samples_x_tsne = np.copy(train_x_tsne[selection])
+
+        for i in range(fixed_cnt):
+            init_samples_x_tsne[i] = old_nodes_tsne[old_nodes_ids.index(selection[i])]
+        samples_x_tsne = IncrementalTSNE(n_components=2, n_jobs=20, init=init_samples_x_tsne, n_iter=250,
+                                         exploration_n_iter=0).fit_transform(samples_x,
+                                                                             skip_num_points=fixed_cnt)
+        return samples_x_tsne
+
+    def get_nodes(self):
+        self.remove_ids = self.model.data.get_removed_idxs()
+        self.tsne = self.get_train_x_tsne()
+        self.hierarchy_info = self.get_hierarchical_sampling()
+        selection = np.array(self.hierarchy_info[0]["index"]).tolist()
+        tsne = self.re_tsne(selection)
+        self.old_nodes_id = selection
+        self.old_nodes_tsne = tsne
+        graph = self.convert_to_dict(selection, tsne)
+        graph["area"] = self.get_data_area(train_x_tsne=tsne)
+        return graph
+
+    def update_nodes(self, area, level, must_show_nodes = []):
+        self.remove_ids = self.model.data.get_removed_idxs()
+        selection, old_cnt = self.get_data_selection(area, level, must_show_nodes)
+        tsne = self.re_tsne(selection, old_cnt)
+        self.old_nodes_id = selection
+        self.old_nodes_tsne = tsne
+        graph = self.convert_to_dict(selection, tsne)
+        graph["area"] = self.get_data_area(train_x_tsne=tsne)
+        return graph
+
+
+    def convert_to_dict(self, selection, tsne):
+        self.wait_for_simplify()
+
+        ground_truth = self.model.data.get_full_train_ground_truth()
+        samples_truth = ground_truth[selection]
+        propagation_path = self.model.propagation_path
+        simplified_affinity_matrix = self.model.simplified_affinity_matrix
+        if self.data_degree is None:
+            self.data_degree = self.model.get_in_out_degree(simplified_affinity_matrix)
+        degree = self.data_degree
+        labels = self.model.labels
+        process_data = self.model.process_data
+
+        samples_x_tsne = tsne.tolist()
+        samples_truth = samples_truth.tolist()
+        samples_nodes = {}
+        sample_num = len(selection)
+        for i in range(sample_num):
+            id = int(selection[i])
+            iter_num = process_data.shape[0]
+            scores = [process_data[j][id].tolist() for j in range(iter_num)]
+            samples_nodes[id] = {
+                "id": id,
+                "x": samples_x_tsne[i][0],
+                "y": samples_x_tsne[i][1],
+                "label": labels[:,id].tolist(),
+                "score": scores,
+                "truth": samples_truth[i],
+                "path": propagation_path[id],
+                "in_degree": int(degree[id][1]),
+                "out_degree": int(degree[id][0])
+            }
+        graph = {
+            "nodes":samples_nodes
+        }
+        return graph
