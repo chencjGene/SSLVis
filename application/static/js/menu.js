609efe70f2f9f687a98f018da15eafaa853cf780
chencjGene <chencjgene@gmail.com>
2020-04-03 20:48:33 +0800

    edit info input

--- application/static/js/menu.js
+++ application/static/js/menu.js
@@ -12,6 +12,7 @@ let EditLayout = function(){
     let focus_mode = null;
     let focus_node = null;
     let edit_mode = null; // TODO: unclear
+    let node_menu_id = null;
     let edit_state = {
             deleted_idxs: [],
             labeled_idxs: [],
@@ -92,39 +93,95 @@ let EditLayout = function(){
                     menu.push(sm);
                 });
             menu.push({
-            title: 'Add',
-            name: '\u2295',
-            color: '',
-            className: "iw-mnotSelected add-menu-item",
-            fun: function () {
-                let new_class = prompt("Please type in new label name:");
-                label_names.push(new_class);
-                name_2_label_map[new_class] = label_names.length-1;
-                menu.splice(menu.length-1, 0, {
-                    title:new_class,
-                    name:new_class,
-                    color: label_colors[label_names.length-1],
-                    className: "iw-mnotSelected label-menu-item",
-                    fun:function(){
-                        let d = new_class;
-                        console.log("click menu", d);
-                            let label = name_2_label_map[d];
-                            that.editing(label);
-                    }
-                });
-                container.contextMenu(menu, click_menu_settings);
-                console.log("add");
-                let label = label_names.length-1;
-                that.editing(label);
+                title: 'Add',
+                name: '\u2295',
+                color: '',
+                className: "iw-mnotSelected add-menu-item",
+                fun: function () {
+                    let this_menu = this;
+                    let ul = $(this_menu).parent();
+                    let css = {
+                        display:ul.css("display"),
+                        position:ul.css("position"),
+                        left:ul.css("left"),
+                        top:ul.css("top")
+                    };
 
-                // let label = -1;
-                // that.editing(label);
-            }
-        });
+                    // let new_class = prompt("Please type in new label name:");
+                    // if(new_class === null) return;
+                    let new_class = "new_class";
+                    let new_class_name = "<input type='text' class='input-new-label'>";
+                    // label_names.push(new_class);
+                    // name_2_label_map[new_class] = label_names.length-1;
+                    menu.splice(menu.length-1, 0, {
+                        title:"new class",
+                        name:new_class_name,
+                        color: label_colors[label_names.length],
+                        className: "iw-mnotSelected label-menu-item new-label",
+                        fun:function(){
+                            let li = $(this);
+                            let is_new_label = $(this).hasClass("new-label");
+                            if(is_new_label) return;
+                            let d = li.attr("title");
+                            console.log("click menu", d);
+                                let label = name_2_label_map[d];
+                                that.editing(label);
+                        }
+                    });
+                    let new_id = container.contextMenu("popup", menu, click_menu_settings);
+                    let new_ul = $(new_id);
+                    new_ul.css("display", css.display);
+                    new_ul.css("position", css.position);
+                    new_ul.css("left", css.left);
+                    new_ul.css("top", css.top);
+                    ul.css("display", "none");
+                    ul.css("position", "");
+                    ul.css("left","");
+                    ul.css("top", "");
+                    $(".new-label").focus();
+                    node_menu_id = new_id;
+                    console.log("new id", new_id);
+                    $(".input-new-label").keypress(function (event) {
+                        let input = $(this);
+                        let keyCode = event.keyCode;
+                        if(keyCode === 13) {
+                            let val = input.val();
+
+                            let li = input.parent();
+                            let newlabel = input.parent().html(val);
+                            li.attr("title", val);
+                            label_names.push(val);
+                            name_2_label_map[val] = label_names.length-1;
+                            li.removeClass("new-label");
+                            menu[menu.length-1] = {
+                                title:val,
+                                name:val,
+                                color: label_colors[label_names.length-1],
+                                className: "iw-mnotSelected label-menu-item",
+                                fun:function(){
+                                    let li = $(this);
+                                    let is_new_label = $(this).hasClass("new-label");
+                                    if(is_new_label) return;
+                                    let d = li.attr("title");
+                                    console.log("click menu", d);
+                                    let label = name_2_label_map[d];
+                                    that.editing(label);
+                                }
+                            }
+                        }
+                    });
+                    // console.log("add");
+                    // let label = label_names.length-1;
+                    // that.editing(label);
+
+                    // let label = -1;
+                    // that.editing(label);
+                }
+            });
 
             click_node_menu = menu;
             if (menu.length > 0) {
-                container.contextMenu(click_node_menu, click_menu_settings);
+                node_menu_id = container.contextMenu(click_node_menu, click_menu_settings);
             }
 
         }
